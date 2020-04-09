fce1c62dea987f43bac7c0dab21cae51d07ac6f5
王好杰 <wanghajie_8866@163.com>
2020-04-09 21:40:30 +0800

    react项目

---
+++ src/components/uc-button/index.js
@@ -0,0 +1,4 @@
+import React,{Component} from 'react';
+
+import styles from './uc-button.module.css'
+import propTypes from 'prop-types';
---
+++ src/pages/Column.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Goods extends React.Component {
+        render() {
+
+            return(
+                <div className='Goods'>
+                    <h3>商品</h3>
+                </div>
+            )
+        }
+}
+
